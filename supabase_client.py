efef4a854c4097cca06a65da5ab71a27d6a8b6ec
Muhammad Sohiburroihan Akbar <bitcoiner.banjarmasin@gmail.com>
2025-08-06 23:39:37 +0800

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -3,7 +3,9 @@ from supabase import create_client
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+// load_dotenv()
+if os.path.exists("/secrets/env-vars/.env"):
+    load_dotenv("/secrets/env-vars/.env")
 
 supabase_url = os.getenv("SUPABASE_URL")
 supabase_key = os.getenv("SUPABASE_KEY")
